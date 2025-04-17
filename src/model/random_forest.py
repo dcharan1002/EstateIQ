abbf0345af27900bf442096c5dcdbf4121ea4613
Shiv <sshivaditya>
2025-04-16 23:24:30 -0400

    fix: rf model

--- src/model/random_forest.py
+++ src/model/random_forest.py
@@ -64,7 +64,7 @@ class RandomForestModel(BaseModel):
                 estimator=RandomForestRegressor(random_state=42),
                 param_distributions=param_grid,
                 n_iter=1,
-                cv=1
+                cv=1,
                 verbose=1,
                 scoring='neg_root_mean_squared_error',
                 random_state=42,
